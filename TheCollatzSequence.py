c6202a64fd84d60c768f5ed9b35e588104be66dd
Pr0M1 <68686888+Pr0M1@users.noreply.github.com>
2020-07-27 15:44:02 +0200

    Update TheCollatzSequence.py

--- TheCollatzSequence.py
+++ TheCollatzSequence.py
@@ -1,14 +1,17 @@
 def collatz(number):
     if number % 2 == 0: #even
         number = number // 2
-        return print(number)
+        print(number)
+        return number 
     elif number % 2 == 1: #odd
         number = 3 * number + 1
-        return print(number)
+        print(number)
+        return number
 
 while True:
-    print("Give my a number:")
-    number = int(input())
-    collatz(number)
-    if number != 1:
-        collatz(number)
+    n = input("Type in a number: ")
+    while True:
+        if n != 1:
+            n = collatz(int(n))
+        elif n == 1:
+            break
