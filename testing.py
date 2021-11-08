b93b99385d654ffe13f454a343524e9bcba47a67
Tenke Dániel <tenke.daniel89@gmail.com>
2021-11-08 13:55:10 +0100

    Update 11.08

--- testing.py
+++ testing.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import os
+
+def textwrithing():
+
+    userr = input("")
+    
+    if userr == "d":
+        openfile = open("C:\\Users\\User\\Desktop\\readme.txt", "a+")
+        openfile.write(input(""))
+
+        openfile = open("C:\\Users\\User\\Desktop\\readme.txt", "r")
+        content = openfile.read()
+        print(content)
