e5e8b8157f2d5d7069ee53167d083564ab7da509
Tenke Dániel <tenke.daniel89@gmail.com>
2022-04-27 11:59:25 +0200

    ready

---
+++ FromScratch.py
@@ -0,0 +1,17 @@
+import random
+
+def whatif():
+
+    press = input()
+
+    if press == "1":
+        print("1")
+    elif press != "1":
+        print("error")
+
+def letsCount():
+
+    count = range(2,5)
+    for n in count:
+        print(n)
+
