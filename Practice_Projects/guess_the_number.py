6cdefd4e055a346d5b3513ad78bbb7e6f68b8448
Tenke Dániel <tenke.daniel89@gmail.com>
2022-10-17 14:17:45 +0200

    update

--- Practice_Projects/guess_the_number.py
+++ Practice_Projects/guess_the_number.py
@@ -1,14 +1,44 @@
 import random
 
-comp_number = random.randint(1, 2)
+def guess_the_number(x):
 
-while True:
+    comp_number = random.randint(1, x)
 
-    my_guess = input("Give me a number: ")
+    guess = 0
 
-    if my_guess == comp_number:
-        print("You guessed my number!")
-    elif my_guess != comp_number:
-        print("Wrong number, try again.")
-    else:
-        print("Give me a number.")
+    while guess != comp_number:
+
+        guess = int(input(f"Give me a number between 1 and {x}: "))
+
+        if guess < comp_number:
+            print("Guess again. Too low.")
+        elif guess > comp_number:
+            print("Guess again. Too High.")
+    
+    print(f"Grat, you guessed {comp_number}")
+
+def comp_guesses(x):
+
+    low = 1
+    high = x
+    feedback = ""
+
+    while feedback != "c":
+
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low 
+
+        feedback = input(f"Is {guess} to high (H), low (L) or or correct (C)?: ")
+
+        if feedback == "h":
+            high = guess -1
+
+        elif feedback == "l":
+            low = guess + 1
+    
+    print(f"Yay! Computer guessed your number, {guess}, correctly")
+
+
+comp_guesses(20)
