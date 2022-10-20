8d9d0935a128e95ce8405ee749332ad59177a8a5
Tenke Dániel <tenke.daniel89@gmail.com>
2022-10-20 14:05:41 +0200

    update

---
+++ Practice_Projects/hangedman.py
@@ -0,0 +1,45 @@
+import random
+from words import word
+import string
+
+def valid_word(words):
+
+    word = random.choice(words)
+
+    while "-" in word or " " in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+
+def hangedman(words):
+
+    word = valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() #what user has guessed
+
+    # getting user input
+    while len(word_letters) > 0:
+        # letters used
+        print("You have used these letters: ", " ".join(used_letters))
+
+        word_list = [letter if letter in used_letters else "-" for letter in word]
+        print("Current word: ", " ".join(word_list))
+
+        user_letter = input("Guess a letter: ").upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+        
+        elif user_letter in used_letters:
+            print("You have already used that character. Please try again. ")
+        
+        else:
+            print("Invalid character. Please try again. ")
+
+
+hangedman(word)
+
+
