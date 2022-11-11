2a5faecf17bf6a5439ff0fbc3647d495a572f0ed
Tenke Dániel <tenke.daniel89@gmail.com>
2022-11-11 14:31:04 +0100

    update

--- Practice_Projects/player.py
+++ Practice_Projects/player.py
@@ -1,5 +1,5 @@
-import math
 import random
+import math
 
 class Player:
 
@@ -28,7 +28,7 @@ class Human_player(Player):
         valid_square = False
         val = None
         while not valid_square:
-            square = input(self.letter + "\'s turn. Input move (0.9)")
+            square = input(self.letter + "\'s turn. Input move (0.9): ")
             # we're going to check that this is a correct value by trying to cast
             # it to an integer, and if it's not, then we say its invalid
             # if that spot is not available on the board, we also say its invalid
@@ -36,6 +36,8 @@ class Human_player(Player):
                 val = int(square)
                 if val not in game. available_moves():
                     raise ValueError
+                valid_square = True
+                
             except ValueError:
                 print("Invalid square. Try again.")
         return val
